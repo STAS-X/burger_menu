44ac55b99860833876fa758e344e44b9ddb5d528
STAS-X <rosteh82@gmail.com>
2024-01-08 19:57:12 +0300

    fix git tracking

--- src/shared/lib/context/ThemeContext.ts
+++
@@ -1,9 +0,0 @@
-import React, { createContext } from 'react';
-import { Theme } from '@/shared/const/theme';
-
-export interface ThemeContextProps {
-	theme?: Theme;
-	setTheme?: (theme: Theme) => void;
-}
-
-export const ThemeContext: React.Context<ThemeContextProps> = createContext<ThemeContextProps>({});
